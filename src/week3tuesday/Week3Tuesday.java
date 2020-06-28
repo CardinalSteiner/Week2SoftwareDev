c4d57a7faa152a83cb22210471cbc6d6927fc711
CardinalSteiner <ranaashu@sheridancollege.ca>
2020-06-28 10:45:48 -0400

    First commit

---
+++ src/week3tuesday/Week3Tuesday.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3tuesday;
+
+/**
+ *
+ * @author Cardinal Steiner
+ */
+public class Week3Tuesday {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Learning the push operation");
+    }
+    
+}
