baabe61d826522ecb1307abe79eba2b9b5ee3160
CardinalSteiner <ranaashu@sheridancollege.ca>
2020-06-28 12:04:22 -0400

    second commit

--- src/week3tuesday/Week3Tuesday.java
+++ src/week3tuesday/Week3Tuesday.java
@@ -17,7 +17,7 @@ public class Week3Tuesday {
     public static void main(String[] args) {
         System.out.println("Learning the push operation");
         System.out.println("Learning to pull from the remote repository");
-        System.out.printn("Learning how to fetch and merge");
+        System.out.println("Learning how to fetch and merge");
     }
     
 }
